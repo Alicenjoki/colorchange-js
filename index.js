1ea3478a3c0f219218e9abfc9952de384ce29613
Alice <alicekamuyu385@gmail.com>
2022-08-13 23:03:30 +0300

    color comit

---
+++ index.js
@@ -0,0 +1,13 @@
+const colors = ["red", "blue", "yellow", "green", "orange"];
+
+const btn = document.getElementById("btn");
+const text = document.querySelector(".color");
+btn.addEventListener('click', function(){
+    const randomNumber = randomN();
+    document.body.style.backgroundColor = colors[randomNumber];
+    text.textContent = colors[randomNumber];
+
+});
+function randomN(){
+    return Math.floor(Math.random() * colors.length);
+}
